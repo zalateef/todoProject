ad8eb09402e735f42dedade105f4ff619b765310
Ziyad <ziyad.a.lateef@gmail.com>
2019-04-29 11:08:30 -0500

    Added Spinner for to show data loading

---
+++ src/components/Spinner.js
@@ -0,0 +1,11 @@
+import React from 'react'
+
+export default function Spinner() {
+  return (
+    <div id="myModal" className="modal">
+        <div className="modal-content">
+            <div className='spinner'></div>
+        </div>
+    </div>
+  )
+}
